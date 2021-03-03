53a5a8570367cd72a173c1338904142e4394f05f
Reneta Ivanova <reneta.iv12@gmail.com>
2021-03-03 18:12:23 +0200

    realized class BronzeCard

--- src/main/BronzeCard.java
+++ src/main/BronzeCard.java
@@ -1,4 +1,19 @@
 package main;
 
 public class BronzeCard extends Card{
+
+    public BronzeCard(String owner, double previousMonthTurnover, double valueOfPurchase) {
+        super(owner, previousMonthTurnover, valueOfPurchase);
+    }
+
+    @Override
+    public double getDiscountRate() {
+        double discountRate = 0;
+        if(this.getPreviousMonthTurnover() >= 100 && this.getPreviousMonthTurnover() <= 300) {
+            discountRate = 1;
+        } else if(this.getPreviousMonthTurnover() > 300) {
+            discountRate = 2.5;
+        }
+        return discountRate;
+    }
 }
