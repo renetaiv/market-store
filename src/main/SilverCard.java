99841addda5954fe133505da0d9a65943e2fd3be
Reneta Ivanova <reneta.iv12@gmail.com>
2021-03-03 18:11:10 +0200

    realized class SilverCard

--- src/main/SilverCard.java
+++ src/main/SilverCard.java
@@ -1,4 +1,16 @@
 package main;
 
 public class SilverCard extends Card{
+
+    public SilverCard(String owner, double previousMonthTurnover, double valueOfPurchase) {
+        super(owner, previousMonthTurnover, valueOfPurchase);
+    }
+
+    @Override
+    public double getDiscountRate() {
+        if(this.getPreviousMonthTurnover() > 300) {
+            return 3.5;
+        }
+        return 2;
+    }
 }
