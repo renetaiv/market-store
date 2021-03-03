bc70d1d2dd32d906a59282ac138885e6c43444b9
Reneta Ivanova <reneta.iv12@gmail.com>
2021-03-03 18:09:24 +0200

    realized class GoldCard

--- src/main/GoldCard.java
+++ src/main/GoldCard.java
@@ -1,4 +1,25 @@
 package main;
 
 public class GoldCard extends Card{
+
+    public GoldCard(String owner, double previousMonthTurnover, double valueOfPurchase) {
+        super(owner, previousMonthTurnover, valueOfPurchase);
+    }
+
+    @Override
+    public double getDiscountRate() {
+        double discountRate = 2;
+        double lastTurnover = this.getPreviousMonthTurnover();
+        if(lastTurnover >= 100) {
+            while (lastTurnover >= 0) {
+                discountRate++;
+                lastTurnover -= 100;
+
+                if(discountRate == 10){
+                    break;
+                }
+            }
+        }
+        return discountRate;
+    }
 }
