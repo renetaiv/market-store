2cca006615ffdf152d741d7d1d78262baba605ad
Reneta Ivanova <reneta.iv12@gmail.com>
2021-03-03 18:14:59 +0200

    realized class Demo

--- src/main/Demo.java
+++ src/main/Demo.java
@@ -3,6 +3,12 @@ package main;
 public class Demo {
 
     public static void main(String[] args) {
+        Card bronzeCard = new BronzeCard("Petar Ivanov", 0, 150);
+        Card silverCard = new SilverCard("Georgi Petkov", 600, 850);
+        Card goldCard = new GoldCard("Kiril Angelov", 1500, 1300);
 
+        bronzeCard.printDetails();
+        silverCard.printDetails();
+        goldCard.printDetails();
     }
 }
